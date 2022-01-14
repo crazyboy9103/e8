1c302097c74bd1abab8a9e591b9a2ea980045cae
crazyboy9103 <gil9103@gmail.com>
2022-01-14 20:28:42 +0900

    docker final

--- MRCNN/train.py
+++ MRCNN/train.py
@@ -2,8 +2,9 @@
 from MRCNN import *
 
 def main():
+    data_dir = "/data/dataset/recsys/e8/data_1230"
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-    dataset = CustomDataset("/data/dataset/recsys/e8/data_1230", "dataset_1230.pt")  
+    dataset = CustomDataset(data_dir, data_dir + "/dataset_1230.pt")  
     model = Model(num_classes=dataset.num_classes, device = device, model_name = "mrcnn_model_65.pt", batch_size=8, parallel=False) # if there is no ckpt to load, pass model_name=None 
     model.fit(dataset, max_epochs=10)
 
--- SSD/train.py
+++ SSD/train.py
@@ -1,8 +1,9 @@
 # +
 from SSD import *
 def main():
+    data_dir = "/data/dataset/recsys/e8/data_1230"
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')     
-    dataset = CustomDataset("/data/dataset/recsys/e8/data_1230", "dataset_1230.pt")
+    dataset = CustomDataset(data_dir, data_dir + "/ssd_data.pt")
     model = Model(num_classes=dataset.num_classes, device = device, model_name = "ssd_model_110.pt", batch_size=128, parallel=False) # if there is no ckpt to load, pass model_name=None 
     model.fit(dataset, max_epochs=20)
 
