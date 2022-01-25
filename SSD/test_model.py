b48cd0a760b54840de833762a34d2ea1b6462be9
crazyboy9103 <gil9103@gmail.com>
2022-01-25 08:48:51 +0000

    final

--- SSD/test_model.py
+++ SSD/test_model.py
@@ -1,4 +1,4 @@
-from MRCNN import *
+from SSD import *
 import argparse
 
 class MyModel(Model):
@@ -10,9 +10,9 @@ class MyModel(Model):
 if __name__ =="__main__":
     parser = argparse.ArgumentParser(description="test model")
     parser.add_argument("--dir", default="/dataset", type=str, help="path to the dataset folder")
-    parser.add_argument("--data", default="mrcnn_data.pt", type=str, help="dataset name to create OR load")
-    parser.add_argument("--model", default="mrcnn_model_10.pt", type=str, help="model name to test")
-    parser.add_argument("--batch", default=8, type=int, help="the batch size")
+    parser.add_argument("--data", default="ssd_data.pt", type=str, help="dataset name to create OR load")
+    parser.add_argument("--model", default="ssd_model_10.pt", type=str, help="model name to test")
+    parser.add_argument("--batch", default=128, type=int, help="the batch size")
 
     args = parser.parse_args()
     
--- SSD/train.py
+++ SSD/train.py
@@ -1,11 +1,20 @@
 # +
 from SSD import *
-def main():
-    data_dir = "/data/dataset/recsys/e8/data_1230"
-    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')     
-    dataset = CustomDataset(data_dir, data_dir + "/ssd_data.pt")
-    model = Model(num_classes=dataset.num_classes, device = device, model_name = "ssd_model_110.pt", batch_size=128, parallel=False) # if there is no ckpt to load, pass model_name=None 
-    model.fit(dataset, max_epochs=20)
+import argparse
+def main(args):
+    data_dir = args.dir #"/dataset"
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')     
+    dataset = CustomDataset(data_dir, data_dir + "/" + args.data)
+    model = Model(num_classes=dataset.num_classes, device = device, model_name = args.model, batch_size=args.batch, parallel=False) # if there is no ckpt to load, pass model_name=None 
+    model.fit(dataset, max_epochs=args.epoch)
 
 if __name__ =="__main__":
-    main()
+    parser = argparse.ArgumentParser(description="train model")
+    parser.add_argument("--dir", default="/dataset", type=str, help="path to the dataset folder")
+    parser.add_argument("--data", default="ssd_data.pt", type=str, help="dataset name to create OR load")
+    parser.add_argument("--model", default="ssd_model_10.pt", type=str, help="model name to begin with")
+    parser.add_argument("--epoch", default=10, type=int, help="number of epochs to train")
+    parser.add_argument("--batch", default=128, type=int, help="the batch size")
+
+    args = parser.parse_args()
+    main(args)
